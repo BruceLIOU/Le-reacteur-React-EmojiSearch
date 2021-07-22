7d0eff0f76996f82a9c458e7fe6906518c76227c
Bruce LIOU <bruceliou@free.fr>
2021-07-22 18:25:34 +0200

    Feature Responsive - Fix limit map

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
 const Footer = () => {
   return (
     <footer>
-      Made width&nbsp;<span>React</span>&nbsp;at&nbsp;<span>Le Reacteur</span>
-      &nbsp;by&nbsp;<span>Bruce LIOU</span>
+      <div>
+        Made width&nbsp;<span>React</span>&nbsp;at&nbsp;<span>Le Reacteur</span>
+        &nbsp;by&nbsp;<span>Bruce LIOU</span>
+      </div>
     </footer>
   );
 };
